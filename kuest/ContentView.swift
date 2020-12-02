0b83f80b68537ca364c4a30e3faffbf41108a16e
YANGJISU <jisuyang@egoist.im>
2020-12-03 06:57:47 +0900

    Initial Commit

---
+++ kuest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kuest
+//
+//  Created by 양지수 on 2020/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kuest/kuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  kuestApp.swift
+//  kuest
+//
+//  Created by 양지수 on 2020/12/03.
+//
+
+import SwiftUI
+
+@main
+struct kuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
