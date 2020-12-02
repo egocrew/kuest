fcb69c8250e2c9142e5cd59693ba3b85bf05d473
JISU-YANG <jisuyang@egoist.im>
2020-12-03 07:34:02 +0900

    카카오 로그인을 위한 iOS SDK 설치
    Cocoapods를 이용하여 KakaoSDKCommon, KakaoSDKAuth, KakaoSDKUser를 받아왔습니다.

---
+++ Pods/Target Support Files/Pods-kuest-kuestUITests/Pods-kuest-kuestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kuest_kuestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kuest_kuestUITests
+@end
---
+++ Pods/Target Support Files/Pods-kuest/Pods-kuest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kuest : NSObject
+@end
+@implementation PodsDummy_Pods_kuest
+@end
